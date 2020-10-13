5264f99d1ff88be3f47fc46ae984e13de1c12a6a
James Churchill <james@smashdev.com>
2020-10-13 12:35:21 -0700

    Update App class

--- src/java/App.java
+++ src/java/App.java
@@ -3,6 +3,9 @@ public class App {
     public static void main(String[] args) {
         System.out.println("hello aimee!!");
         System.out.println("hello again aimee!! -Logan");
+
+        System.out.println("Two bytes meet. The first byte asks, \"Are you ill?\"");
+        System.out.println("The second byte replies, \"No, just feeling a bit off.\"");
     }
 
 }
