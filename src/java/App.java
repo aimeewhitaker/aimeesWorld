2262bec1a857f1f7a5bbde3d9f32b84f28f9fc01
LoganLang <langloganr@gmail.com>
2020-10-13 10:09:16 -0500

    Logan's commit

--- src/java/App.java
+++ src/java/App.java
@@ -2,6 +2,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("hello aimee!!");
+        System.out.println("hello again aimee!! -Logan");
     }
 
 }
